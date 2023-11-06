257c36ed1699138ad7c2f4e9cf317be46c003fdb
Ryufath Alief Adhyaksa Putera Soepeno <89006072+ryu0210@users.noreply.github.com>
2023-11-06 11:53:46 +0700

    Update F. Integrity & KEY Constraints.sql

--- F. Integrity & KEY Constraints.sql
+++ F. Integrity & KEY Constraints.sql
@@ -3,7 +3,7 @@ CREATE TABLE Employees (
   employee_id INT PRIMARY KEY,
 
 /** Primary Key:
-The PRIMARY KEY constraint uniquely identifies each row in a table. 
+The PRIMARY KEY constraint "uniquely identifies" each row in a table. 
 It ensures that the values in the specified column(s) are unique and not null.
 
 In the example above, the employee_id column is defined as the primary key using the PRIMARY KEY constraint. 
@@ -17,7 +17,7 @@ It guarantees that each employee has a "unique" ID.
   UNIQUE (first_name, last_name),
   
   /** Unique Key:
-The UNIQUE constraint ensures that the values in the specified column(s) are unique across all rows in the table.
+The UNIQUE constraint ensures that the values in the specified column(s) are "unique across all rows" in the table.
 
 In the example above, the combination of first_name and last_name columns is defined as a unique key using the UNIQUE constraint. 
 It guarantees that each employee's first name and last name "combination" is "unique".
@@ -26,7 +26,7 @@ It guarantees that each employee's first name and last name "combination" is "un
   FOREIGN KEY (department_id) REFERENCES Departments (department_id)
   
   /** Foreign Key:
-The FOREIGN KEY constraint establishes a link between two tables based on a column(s). 
+The FOREIGN KEY constraint "establishes a link" between two tables based on a column(s). 
 It ensures that the values in the specified column(s) in one table match the values in the referenced column(s) in another table.
 
 In the example above, the department_id column in the Employees table is defined as a foreign key using the FOREIGN KEY constraint. 
